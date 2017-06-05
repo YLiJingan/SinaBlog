b06e7e2b291ad5376aea523446295a398f1a599b
YLiJingan <954047253@qq.com>
2017-06-05 09:40:57 +0800

    update

---
+++ blog.js
@@ -0,0 +1,8 @@
+/**
+ * Created by yan on 2017/6/4.
+ */
+window.onload = function(){
+    //alert($().getId('box').css('backgroundColor'));
+    //alert($().getClass('box').elements.length);
+    $().getClass('box').getElement(2).css('color','green');
+}
