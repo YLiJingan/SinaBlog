0436a4c8de5f722110a18471cb7f051fe8c95d23
YLiJingan <954047253@qq.com>
2017-06-12 21:26:12 +0800

    update

--- js/tool.js
+++ js/tool.js
@@ -1,6 +1,64 @@
 /**
  * Created by yan on 2017/6/11.
  */
+//跨浏览器事件绑定
+function addEvent(obj,type,fn){
+    if(obj.addEventListener != 'undefined'){
+        obj.addEventListener(type,fn,false);
+    }else{
+        if(!obj.events) obj.events = {};
+        if(!obj.events[type]){
+            obj.events[type] = [];
+            if(obj['on' + type]) obj.events[type][0] = fn;
+        }else{
+            if(addEvent.equal(obj.events[type], fn)) return false;
+        }
+        obj.events[type][addEvent.ID++] =fn;
+        obj['on' + type] = addEvent.exec;
+    }
+}
+addEvent.ID = 1;
+//执行事件处理函数
+addEvent.exec = function(event){
+    var e = event || window.event;
+    var es = this.events[e.type];
+    for(var i in es){
+        es[i].call(this,e);
+    }
+};
+//同一个注册函数进行屏蔽
+addEvent.equal = function(es,fn){
+    for(var i in es){
+        if(es[i] == fn) return true;
+    }
+    return false;
+}
+//把IE常用的Event对象配对到W3C中去
+addEvent.fixEvent = function(event){
+    event.preventDefault = addEvent.fixEvent.preventDefault;
+    event.stopPropagation = addEvent.fixEvent.stopPropagation;
+    return event;
+}
+//IE阻止默认行为
+addEvent.fixEvent.preventDefault = function (){
+    this.returnValue = false;
+}
+//IE取消冒泡
+addEvent.fixEvent.stopPropagation = function(){
+    this.cancelBubble = true;
+}
+//跨浏览器取消事件
+function removeEvent(obj,type,fn){
+    if(obj.removeEventListener != 'undefined'){
+        obj.removeEventListener(type,fn,false);
+    }else{
+        for(var i in obj.events[type]){
+            if(obj.events[type][i] == fn){
+                delete obj.events[type][i];
+            }
+        }
+    }
+}
 
 //跨浏览器获取视口大小
 function getInner(){
